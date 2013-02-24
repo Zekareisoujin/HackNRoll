5cf58b9abbd4953b58025d61277d1672e05be066
Nguyen Phi Long <Zeka304@yahoo.com>
2013-02-24 15:07:49 +0800

    Initial rail setup

---
+++ trhunt/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trhunt::Application.config.secret_token = '855cee3b687494aaa4c5d906d16912a0985df01f3141b28d24221421620266a4391cc20bccec1f73a9082da1a626c2fd6b7ddbf105c09a84a4abd90167c007ef'
